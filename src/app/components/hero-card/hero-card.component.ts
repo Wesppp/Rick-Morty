977a469f3749309be0164f4c2159a49fc2306d62
wesppp <mshesnovich@mail.ru>
2024-03-14 13:48:50 +0300

    create location-tooltip for hero card

--- src/app/components/hero-card/hero-card.component.ts
+++ src/app/components/hero-card/hero-card.component.ts
@@ -1,16 +1,25 @@
 import { CommonModule } from '@angular/common';
-import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  Input,
+  computed,
+  inject,
+} from '@angular/core';
+
+import { TooltipModule } from 'primeng/tooltip';
+import { toObservable } from '@angular/core/rxjs-interop';
 
 import { RICK } from '@constants/rick-hero-mock';
 import { HeroStatus } from '@enums/hero-status.enum';
 import { Hero } from '@models/hero.interface';
+import { LocationTooltipComponent } from './components/location-tooltip/location-tooltip.component';
+import { HeroesStore } from '@store/heroes.store';
 
 @Component({
   selector: 'app-hero-card',
   standalone: true,
-  imports: [
-    CommonModule,
-  ],
+  imports: [CommonModule, TooltipModule, LocationTooltipComponent],
   templateUrl: './hero-card.component.html',
   styleUrl: './hero-card.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
@@ -18,5 +27,14 @@ import { Hero } from '@models/hero.interface';
 export class HeroCardComponent {
   @Input() public hero: Hero = RICK;
 
+  protected readonly store = inject(HeroesStore);
+
+  public heroLocation = toObservable(computed(() => this.store.allHeroesLocation()[this.hero.id]));
   protected heroStatus = HeroStatus;
+
+  constructor() {}
+
+  public getHeroLocation(url: string, heroId: number): void {
+    this.store.getHeroLocation({ url, heroId });
+  }
 }
