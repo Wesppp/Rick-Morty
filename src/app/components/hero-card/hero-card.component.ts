d2a90ef826c3e2f8210dfedcd8ee5dfc6d3c880a
wesppp <mshesnovich@mail.ru>
2024-03-14 16:16:24 +0300

    create episodes-tooltip

--- src/app/components/hero-card/hero-card.component.ts
+++ src/app/components/hero-card/hero-card.component.ts
@@ -1,11 +1,5 @@
 import { CommonModule } from '@angular/common';
-import {
-  ChangeDetectionStrategy,
-  Component,
-  Input,
-  computed,
-  inject,
-} from '@angular/core';
+import { ChangeDetectionStrategy, Component, Input, computed, inject } from '@angular/core';
 
 import { TooltipModule } from 'primeng/tooltip';
 import { toObservable } from '@angular/core/rxjs-interop';
@@ -15,11 +9,12 @@ import { HeroStatus } from '@enums/hero-status.enum';
 import { Hero } from '@models/hero.interface';
 import { LocationTooltipComponent } from './components/location-tooltip/location-tooltip.component';
 import { HeroesStore } from '@store/heroes.store';
+import { EpisodesTooltipComponent } from './components/episodes-tooltip/episodes-tooltip.component';
 
 @Component({
   selector: 'app-hero-card',
   standalone: true,
-  imports: [CommonModule, TooltipModule, LocationTooltipComponent],
+  imports: [CommonModule, TooltipModule, LocationTooltipComponent, EpisodesTooltipComponent],
   templateUrl: './hero-card.component.html',
   styleUrl: './hero-card.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
@@ -30,11 +25,14 @@ export class HeroCardComponent {
   protected readonly store = inject(HeroesStore);
 
   public heroLocation = toObservable(computed(() => this.store.allHeroesLocation()[this.hero.id]));
+  public episodes = toObservable(computed(() => this.store.allHeroesEpisodes()[this.hero.id]));
   protected heroStatus = HeroStatus;
-
-  constructor() {}
 
   public getHeroLocation(url: string, heroId: number): void {
     this.store.getHeroLocation({ url, heroId });
+  }
+
+  public getHeroEpisodes(urls: string[], heroId: number): void {
+    this.store.getHeroEpisodes({ urls, heroId });
   }
 }
--- src/app/services/hero.service.ts
+++ src/app/services/hero.service.ts
@@ -1,9 +1,10 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
-import { Observable } from 'rxjs';
+import { Observable, forkJoin } from 'rxjs';
 
 import { HeroLocation } from '@models/location.interface';
+import { Episode } from '@models/episode.interface';
 
 @Injectable({
   providedIn: 'root',
@@ -13,5 +14,11 @@ export class HeroService {
 
   public getHeroLocation(url: string): Observable<HeroLocation> {
     return this.http.get<HeroLocation>(url);
+  }
+
+  public getHeroEpisodes(urls: string[]): Observable<Episode[]> {
+    const req = urls.map(url => this.http.get<Episode>(url));
+
+    return forkJoin<Episode[]>(req);
   }
 }
