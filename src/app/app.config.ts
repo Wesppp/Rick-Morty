2e97623ac778ba2f22a31e281342027620054937
wesppp <mshesnovich@mail.ru>
2024-03-20 12:42:44 +0300

    replace BASE_URL token with env file

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,11 +4,9 @@ import { provideAnimations } from '@angular/platform-browser/animations';
 import { provideHttpClient } from '@angular/common/http';
 
 import { routes } from './app.routes';
-import { BASE_URL } from '@constants/tokens';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    {provide: BASE_URL, useValue: 'https://rickandmortyapi.com/api'},
     provideRouter(routes),
     provideAnimations(),
     provideHttpClient()
--- src/app/services/hero.service.ts
+++ src/app/services/hero.service.ts
@@ -1,23 +1,21 @@
 import { HttpClient } from '@angular/common/http';
-import { Inject, Injectable } from '@angular/core';
+import { Injectable } from '@angular/core';
 
 import { Observable, forkJoin } from 'rxjs';
 
 import { HeroLocation } from '@models/location.interface';
 import { Episode } from '@models/episode.interface';
-import { BASE_URL } from '@constants/tokens';
 import { Hero } from '@models/hero.interface';
 import { HeroesResponse } from '@models/heroes-response.interface';
 import { mapResponseToResult } from '@utils/mappers';
+import { environment } from '@environments/environment.development';
 
 @Injectable({
   providedIn: 'root',
 })
 export class HeroService {
   constructor(
-    private readonly http: HttpClient,
-    @Inject(BASE_URL) private readonly url: string,
-  ) {}
+    private readonly http: HttpClient) {}
 
   public getHeroLocation(url: string): Observable<HeroLocation> {
     return this.http.get<HeroLocation>(url);
@@ -30,11 +28,11 @@ export class HeroService {
   }
 
   public getHeroes(): Observable<Hero[]> {
-    return this.http.get<HeroesResponse>(`${this.url}/character`)
+    return this.http.get<HeroesResponse>(`${environment.apiUrl}/character`)
       .pipe(mapResponseToResult());
   }
 
   public getMultipleHeroes(page: number): Observable<HeroesResponse> {
-    return this.http.get<HeroesResponse>(`${this.url}/character/?page=${page}`);
+    return this.http.get<HeroesResponse>(`${environment.apiUrl}/character/?page=${page}`);
   }
 }
